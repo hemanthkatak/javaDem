38a7e531e13fc488baaad47bc7a2c2ad0f9bca0c
venka <venka@10.0.0.228>
2024-02-13 21:10:57 -0500

    java program

---
+++ Javademo/src/javaday1/clasname.java
@@ -0,0 +1,30 @@
+package javaday1;
+
+
+public class clasname {
+
+	/*static void method1() {
+		int a=60;
+		int b=30;
+		int c=a+b;*/
+		
+	
+		//String name="Hemanth";
+		//System.out.println(name);
+		//System.out.println(c +" is my value");
+	
+	    
+	public static void main(String[] args) {
+		
+		//clasname object=new clasname();
+		//object.method1();
+		int i=1;
+		while(i<5);
+	{
+      System.out.println("hemanth");
+      i++;
+	}
+}
+}	
+
+
